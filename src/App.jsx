9c17bf2bb2cb71713993984e31ff3391d613e399
gangasandhu <sr.gangasingh@gmail.com>
2024-03-17 03:04:49 -0700

    #1: react basic setup created

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './styles/App.css'
+import './styles/bootstrap.min.css'
+
+function App() {
+
+  return (
+    <>
+      Ganga's personal portfolio
+    </>
+  )
+}
+
+export default App
