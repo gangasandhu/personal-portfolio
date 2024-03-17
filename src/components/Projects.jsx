a1053dad1196630479bc9d7f2e3921fa6a57041d
gangasandhu <sr.gangasingh@gmail.com>
2024-03-17 13:15:18 -0700

    #17: images updated in profile section

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -9,7 +9,7 @@ const Projects = () => {
 
                 <div class="col-md-4">
                     <div class="card">
-                        <img src="https://picsum.photos/200/100" class="card-img-top" alt="Project 1 Image" />
+                        <img src="https://source.unsplash.com/random/200×200/?coding" width="200" height="200"  class="card-img-top" alt="Project 1 Image" />
                         <div class="card-body">
                             <h5 class="card-title">SmartScript</h5>
                             <p class="card-text">AI-assisted code-learning application using React. The application integrates a code editor, a compiler, and an AI assistant to provide a comprehensive and interactive learning environment for coding enthusiasts.</p>
@@ -20,7 +20,7 @@ const Projects = () => {
 
                 <div class="col-md-4">
                     <div class="card">
-                        <img src="https://picsum.photos/200/100" class="card-img-top" alt="Project 2 Image" />
+                        <img src="https://source.unsplash.com/random/200×200/?website" width="200" height="200" class="card-img-top" alt="Project 2 Image" />
                         <div class="card-body">
                             <h5 class="card-title">Blog App</h5>
                             <p class="card-text">A blog app created using Django The app includes several features such as user authentication, profile creation, blog post creation, updating and deleting, commenting, and pages for blog posts.</p>
@@ -31,7 +31,7 @@ const Projects = () => {
 
                 <div class="col-md-4">
                     <div class="card">
-                        <img src="https://picsum.photos/200/100" class="card-img-top" alt="Project 3 Image" />
+                        <img src="https://source.unsplash.com/random/200×200/?videogame" width="200" height="200"  class="card-img-top" alt="Project 3 Image" />
                         <div class="card-body">
                             <h5 class="card-title">Frogger Game</h5>
                             <p class="card-text">Frogger Game built using javascript. A broswer based video game where player have to control a frog and cross road and a river without being hit or drawn. The objective is to reach the safehouses.</p>
